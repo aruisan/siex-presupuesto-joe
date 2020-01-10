23ca03a52dcb3ed9ae17f2f7cc4e85d572afe36e
DeivithG <deivith.1@hotmail.com>
2020-01-10 17:59:48 -0500

    cambios visuales, se agregan los nombres de la mesa directiva completa

--- resources/views/visitante/modal/presupuesto.blade.php
+++ resources/views/visitante/modal/presupuesto.blade.php
@@ -5,7 +5,7 @@
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title text-center">
-                        Presupuesto Año 2019
+                        Presupuesto Año {{ $añoActual }}
                     </h4>
                 </div>
               <div class="modal-body">
