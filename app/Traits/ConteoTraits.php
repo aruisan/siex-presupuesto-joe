845dbc06f06e9867d25ec929c038d425babbec1f
aruisan <oskrjag@gmail.com>
2020-01-14 11:40:08 -0500

    arreglo de vigencia 2019 conteo cdps

--- app/Traits/ConteoTraits.php
+++ app/Traits/ConteoTraits.php
@@ -6,7 +6,12 @@ use App\Model\Hacienda\Presupuesto\Vigencia;
 Class ConteoTraits
 {
 	public function conteoCdps($vigencia, $cdp_id){
-        $vigenciaOld = Vigencia::where('vigencia',$vigencia-1)->where('tipo', 0)->first();
-        return $cdp_id-$vigenciaOld->cdps->last()->id;
+        $conteo = $cdp_id;
+        if($vigencia > 2019){
+            $vigenciaOld = Vigencia::where('vigencia',$vigencia-1)->where('tipo', 0)->first();
+            $conteo = $cdp_id-$vigenciaOld->cdps->last()->id;
+        }
+
+        return $conteo;
     }
 }
